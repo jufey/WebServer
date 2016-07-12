4e1549220137066eed5e30b06033e48454c7af81
justin <justin_m@live.de>
2016-07-12 11:53:33 +0200

    testCommit

--- src/WebServer.java
+++ src/WebServer.java
@@ -20,6 +20,7 @@ public final class WebServer {
         double test2;
         double test3;
         double test4;
+        double test5;
 
         //For each incoming connection a new HttpRequest object (Finished)
         boolean running = true;
